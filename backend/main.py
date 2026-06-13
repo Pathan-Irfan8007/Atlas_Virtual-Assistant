d629c5825e706d69cdfeec87e2306615cfe17756
Pathan-Irfan8007 <pathanirfan8007@gmail.com>
2026-06-13 10:24:09 +0530

    Process Command

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 import pyttsx3
 import speech_recognition as sr
+import time
+import webbrowser
+import keyboard
+import pyautogui
+import subprocess
 
 
+engine = pyttsx3.init()
 def speak(text):
-    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
@@ -23,17 +28,47 @@ def take_command():
     except Exception :
         return ""
 
+def process_command(command):
+    
+    command = command.lower()
+
+    if "say hello" in command:
+        speak("Hello friend, Nice to meet you !")
+    elif "open google" in command:
+        webbrowser.open("https://www.google.com")
+    elif "open youtube" in command:
+        webbrowser.open("https://www.youtube.com")
+    elif "minimise" in command or "maximize" in command:
+        keyboard.press_and_release("win + d")
+    elif "scroll down" in command:
+        pyautogui.scroll(-500)
+    elif "scroll up" in command:
+        pyautogui.scroll(500)
+    elif "open notepad" in command:
+        subprocess.Popen("notepad.exe")
+    else:
+        speak("Sorry but I am not prepared for that .")
+
+
+
 if __name__ == "__main__" :
-    print("Hello I am Atlas, a virtual assistant")
-    speak("Hello I am Atlas, a virtual assistant")
-
+    
     while True:
-        print()
-        command = take_command()
+        initialize = take_command()
+        if "wake up" in initialize.lower():
+            print("Hello I am Atlas, a virtual assistant")
+            speak("Hello I am Atlas, a virtual assistant")
+            
 
-        if command :
-            print(f"This is Your Command : {command}")
-            speak(f"This is Your Command : {command}")
-        else:
-            print("No command recognize !")
-            speak("No command recognize !")
+            while True:
+                print()
+                command = take_command()
+
+                if command :
+                    # print(command)
+                    process_command(command)
+                else:
+                    print("No command recognize !")
+                    speak("No command recognize !")
+
+        
